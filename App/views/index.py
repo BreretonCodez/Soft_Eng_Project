de8bb0a06522d1f2d2547b1feba54e36949ba110
BreretonCodez <micahbrereton@hotmail.com>
2022-11-08 22:46:06 +0000

    Update frontend

--- App/views/index.py
+++ App/views/index.py
@@ -1,7 +1,20 @@
 from flask import Blueprint, redirect, render_template, request, send_from_directory
 
+from App.controllers import (
+    get_all_publications,
+    get_all_authors,
+)
+
 index_views = Blueprint('index_views', __name__, template_folder='../templates')
 
 @index_views.route('/', methods=['GET'])
 def index_page():
-    return render_template('index.html')
+    search = request.args.get('search')
+    if search:
+        '''publications = search_pub(search)'''
+        authors = get_all_authors()
+        return render_template('index.html', publications=publications, authors=authors)
+    else:
+        publications = get_all_publications()
+        authors = get_all_authors()
+        return render_template('index.html', publications=publications, authors=authors)
