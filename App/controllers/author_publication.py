fa1c1fc4496be3e04eb1f2bd041e2e474aa2d634
zanedotmp4 <zaneamour@gmail.com>
2022-11-22 23:00:50 +0000

    update comments

--- App/controllers/author_publication.py
+++ App/controllers/author_publication.py
@@ -1,6 +1,6 @@
 from App.models import AuthorPublication
 from App.database import db
-
+'''get all items in the form of a json'''
 def get_all_items_json():
     rows = AuthorPublication.query.all()
     if not rows:
