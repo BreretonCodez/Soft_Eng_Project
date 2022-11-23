70df6c2ea4b37afbc65651a442c74a61762429b1
BreretonCodez <micahbrereton@hotmail.com>
2022-11-23 00:28:26 +0000

    deploy to production database

--- App/config.py
+++ App/config.py
@@ -1,4 +1,4 @@
-SQLALCHEMY_DATABASE_URI = "sqlite:///temp-database.db"
+SQLALCHEMY_DATABASE_URI = "postgresql://iwglokcnugudui:9f627b65dc6d1ff6083609218e1e30a44aa6684444a7ab4986e9d4c2aeef890a@ec2-34-201-95-176.compute-1.amazonaws.com:5432/d6jq8j2novtivd"
 SECRET_KEY = "secret key"
 JWT_EXPIRATION_DELTA = 7
 ENV = "DEVELOPMENT"
