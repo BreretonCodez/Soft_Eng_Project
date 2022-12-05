3cd6ffd1c96b5731c6b61c070dbeecf22b689374
BreretonCodez <micahbrereton@hotmail.com>
2022-12-05 04:05:15 +0000

    update user controller

--- App/controllers/user.py
+++ App/controllers/user.py
@@ -38,7 +38,7 @@ def search_user(search):
 ''' Author Accessors by parameter '''
 
 def get_user_by_id(id):
-    return User.query.filter_by(id=id).first()
+    return User.query.filter_by(userId=id).first()
 
 def get_user_by_username(username):
     return User.query.filter_by(username=username).first()
