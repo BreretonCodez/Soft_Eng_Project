46674ca93c921cbe53480512d9b039eef8dad50b
BreretonCodez <micahbrereton@hotmail.com>
2022-12-07 02:14:03 +0000

    update pub unit test

--- App/tests/test_app.py
+++ App/tests/test_app.py
@@ -74,18 +74,9 @@ class AuthorUnitTests(unittest.TestCase):
 class PublicationUnitTests(unittest.TestCase):
     def test_new_publication(self):
         #(title, author, link, content, publisher, year)
-        coauthor = Author("James", "Band", "jband@spy.net")
         publication = Publication("Properly Using Git for Noobs", 1, "https://git.github.com", "Git is a powerful tool that everyone should know how to use.", "UWI", 2022)
         publication.add_co_author(coauthor)
-        assert (
-            publication.title=="Properly Using Git for Noobs"
-            and publication.link=="https://git.github.com"
-            and publication.content=="Git is a powerful tool that everyone should know how to use." 
-            and publication.publisher=="UWI"
-            and publication.author==1
-            and publication.coauthors==coauthor
-            and publication.year==2022
-        )
+        assert publication.title=="Properly Using Git for Noobs" and publication.link=="https://git.github.com" and publication.content=="Git is a powerful tool that everyone should know how to use." and publication.publisher=="UWI" and publication.author==1 and publication.coauthors==[] and publication.year==2022
 
     def test_publication_toJSON(self):
         author = Author("James", "Bond", "jbond@spy.net")
